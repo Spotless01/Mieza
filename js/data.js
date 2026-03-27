f1bc4e27083ceb72130fa90caa319150c60d3fdc
Spotless01 <emmanuelmieza01@gmail.com>
2026-03-27 00:07:58 +0000

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,54 @@
+const shops = [
+  {
+    id: "gracious-classic",
+    name: "Gracious Classic Fabric Hub",
+    category: "Fashion 🧵",
+    thumbnail: "images/shops/gracious_classic.jpg",
+
+    deliveryTime: "less than 30 mins delivery",
+    deliveryFee: 20,
+
+    openingHours: { open: "06:00", close: "23:59" },
+
+    products: [
+      {
+        id: 1,
+        name: "Esterica Print (6 yards)",
+        price: 450,
+        image: "images/products/gracious_classic_fabric/cloth1.png"
+      },
+      {
+        id: 2,
+        name: "Esterica Print",
+        price: 450,
+        image: "images/products/gracious_classic_fabric/cloth2.png"
+      },
+      {
+        id: 3,
+        name: "Esterica Print",
+        price: 450,
+        image: "images/products/gracious_classic_fabric/cloth3.png"
+      },
+      {
+        id: 4,
+        name: "Esterica Print",
+        price: 450,
+        image: "images/products/gracious_classic_fabric/cloth4.png"
+      },
+      {
+        id: 5,
+        name: "Esterica Print",
+        price: 450,
+        image: "images/products/gracious_classic_fabric/cloth5.png"
+      },
+      {
+        id: 6,
+        name: "Esterica Print",
+        price: 450,
+        image: "images/products/gracious_classic_fabric/cloth6.png"
+      },
+    ],
+
+    feedback: []
+  }
+];
