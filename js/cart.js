a8d6068028f5a863557f4d2c495c9a11cfaf1795
Spotless01 <emmanuelmieza01@gmail.com>
2026-05-26 13:23:20 +0000

    Connected frontend to live backend

--- js/cart.js
+++ js/cart.js
@@ -36,7 +36,7 @@ function addToCart(shopId, product, btn) {
       price: product.price,
       image: product.image.startsWith("http")
   ? product.image
-  : `https://mieza.onrender.com${product.image}`,
+  : `https://mieza.onrender.com/api${product.image}`,
       qty: 1
     });
   }
