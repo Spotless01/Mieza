27ee2d61ee9a8afb4ff6836fa189efe681c38c3d
Spotless01 <emmanuelmieza01@gmail.com>
2026-06-01 13:10:47 +0000

    Use Brevo SSL port

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -1,12 +1,9 @@
 const nodemailer = require("nodemailer");
 
-console.log("EMAIL USER:", process.env.EMAIL_USER);
-console.log("EMAIL PASS EXISTS:", !!process.env.EMAIL_PASS);
-
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
