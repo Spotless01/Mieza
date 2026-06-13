0749c863a314e1286ddb5f9263499afabc2ec3d5
Spotless01 <emmanuelmieza01@gmail.com>
2026-06-13 23:42:46 +0000

    Connected frontend to live backend

--- js/checkout.js
+++ js/checkout.js
@@ -134,7 +134,38 @@ document.getElementById(
 ${latitude.toFixed(5)},
 ${longitude.toFixed(5)}`;
 
+try {
 
+  const response =
+    await fetch(
+
+`https://api.mapbox.com/geocoding/v5/mapbox.places/${longitude},${latitude}.json?access_token=${mapboxgl.accessToken}`
+
+    );
+
+  const data =
+    await response.json();
+
+  if (
+    data.features &&
+    data.features.length
+  ) {
+
+    document.getElementById(
+      "location"
+    ).value =
+      data.features[0].place_name;
+
+  }
+
+} catch (err) {
+
+  console.log(
+    "Reverse geocoding failed:",
+    err
+  );
+
+}
 
 },
 
